e40fcc3798582da059e35f733e4a48b30f720ae1
Sahil Shrestha <sahiltuchhe123@gmail.com>
2024-10-19 17:49:01 +0545

    React  Router Crash course

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+  
+    </>
   )
 }
 
